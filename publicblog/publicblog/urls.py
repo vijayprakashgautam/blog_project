aefa85a11d80111f206f0e3d6666d0cccc0bc621
vijayprakashgautam <vijayprakashgautam2681@gmail.com>
2020-02-02 16:52:02 +0530

    url added for blog app

--- publicblog/publicblog/urls.py
+++ publicblog/publicblog/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
