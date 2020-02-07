536435903b34f311391203c85d363168f5acb05e
vijayprakashgautam <vijayprakashgautam2681@gmail.com>
2020-02-08 00:43:51 +0530

    urlpatterns modified with django latest version

--- publicblog/blog/urls.py
+++ publicblog/blog/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from . import views
+from blog import views
 
 
-pathpatterns = [
+urlpatterns = [
     path('',views.PostListView.as_view(),name='post_list'),
     path('about/',views.AboutView.as_view(),name='about'),
-    path('post/(?P<pk>\d+)', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
-    path('post/(?P<pk>\d+)/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
-    path('post/(?P<pk>\d+)/remove/', views.PostDeleteView.as_view(), name='post_remove'),
-    path('post/(?P<pk>\d+)/publish/', views.post_publish, name='post_publish'),
-    path('post/(?P<pk>\d+)/comment/', views.add_comment_to_post, name='add_comment_to_post'),
-    path('comment/(?P<pk>\d+)/approve/', views.comment_approve, name='comment_approve'),
-    path('comment/(?P<pk>\d+)/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
