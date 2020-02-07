b6cdd6b64038e36cb5ab5b71ad673a6a4405f4b0
vijayprakashgautam <vijayprakashgautam2681@gmail.com>
2020-02-08 00:39:15 +0530

    urlpatterns updated by django latest version

--- publicblog/publicblog/urls.py
+++ publicblog/publicblog/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView,LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/login/', views.login, name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
